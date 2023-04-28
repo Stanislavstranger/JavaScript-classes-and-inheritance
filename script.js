56cc9453eb812c6b6ea3be3ae28b00e7316513b2
Stanislav Goloborodov <stanislavstranger@gmail.com>
2023-04-28 11:39:29 +0300

    feat: add in ES5: all methods

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ class BaseBuilder {
         return this.value;
     }
 
+    plus(...n) {
+        this.value = n.reduce((sum, n) => {
+            return sum + n;
+        }, this.value);
+        return this;
+    }
+
 }
 
 /* create child class (inherit from base): IntBuilder in ES6 style */
@@ -18,13 +25,6 @@ class IntBuilder extends BaseBuilder {
 
     constructor(int) {
         super(int);
-    }
-
-    plus(...int) {
-        this.value = int.reduce((sum, int) => {
-            return sum + int;
-        }, this.value);
-        return this;
     }
 
     minus(...int) {
@@ -61,13 +61,6 @@ class IntBuilder extends BaseBuilder {
 }
 
 let intBuilder = new IntBuilder(10); // 10;
-/* intBuilder
-  .plus(2, 3, 2)                     // 17;
-  .minus(1, 2)                       // 14;
-  .multiply(2)                       // 28;
-  .divide(4)                         // 7;
-  .mod(3)                            // 1;
-  .get(); */
 
 console.log(intBuilder.plus(2, 3, 2));
 console.log(intBuilder.minus(1, 2));
@@ -75,8 +68,58 @@ console.log(intBuilder.multiply(2));
 console.log(intBuilder.divide(4));
 console.log(intBuilder.mod(3));
 console.log(intBuilder.exp(3));
+console.log(intBuilder.get());
 console.log(IntBuilder.random(10, 100));
 
 /* create child class (inherit from base): StringBuilder in ES5 style */
 
+function StringBuilder(str = "") {
+    this.value = str;
+}
 
+StringBuilder.prototype = Object.create(BaseBuilder.prototype);
+StringBuilder.prototype.constructor = StringBuilder;
+
+StringBuilder.prototype.minus = function (...str) {
+    this.value = this.value.slice(0, -str);
+    return this;
+}
+
+StringBuilder.prototype.multiply = function (...str) {
+    this.value = this.value.repeat(str);
+    return this;
+}
+
+StringBuilder.prototype.divide = function (...str) {
+    this.value = this.value.slice(0, Math.floor(this.value.length / str));
+    return this;
+}
+
+StringBuilder.prototype.remove = function (...str) {
+    this.value = this.value.split(str).join("").trim();
+    return this;
+}
+
+StringBuilder.prototype.sub = function (from, length) {
+    this.value = this.value.slice(from, from + length);
+    return this;
+}
+
+let strBuilder = new StringBuilder('Hello'); // 'Hello';
+
+/* strBuilder
+  .plus(' all', '!')                         // 'Hello all!'
+  .minus(4)                                  // 'Hello '
+  .multiply(3)                               // 'Hello Hello Hello '
+  .divide(4)                                 // 'Hell';
+  .remove('l')                               // 'He';
+  .sub(1,1)                                  // 'e';
+  .get();                                    // -> 'e'; */
+
+console.log(strBuilder.plus(' all', '!'));
+console.log(strBuilder.minus(4));
+console.log(strBuilder.multiply(3));
+console.log(strBuilder.divide(4));
+console.log(strBuilder.remove('l'));
+console.log(strBuilder.sub(1, 1));
+console.log(strBuilder.get());
